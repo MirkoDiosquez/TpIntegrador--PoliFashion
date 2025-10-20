6c9e9968c8ee26582c9d71aaefac595f91f470bb
MaximoH <alumno26.hermosilla.maximo@ipm.edu.ar>
2025-10-20 12:38:21 -0300

    Faltan 3 querys SQL

--- DHT Querys.sql
+++ DHT Querys.sql
@@ -1,7 +1,4 @@
 
-
-
-
 -- Ejercicio 3 --
 
 delimiter //
@@ -15,10 +12,63 @@ begin
 
 call prendasMasVendidas ('2025-10-10','2025-10-12');
 
--- Ejercicio 4 --
+
+delimiter //
+-- Ejercicio 4  Falta terminar--
 create procedure devolucionPrenda (in idVenta int, in dniClient varchar())
 begin	
 	if ( select idCompra from compra where idCompra = idVenta ) then
+    end;
     
+    
+    end;
+//
 
 
+-- Ejercicio 5--
+
+delimiter //
+create procedure bonusClientes () 
+begin
+	declare fechaFin date;
+	declare fechaInicio date;
+    set fechaInicio = curdate();
+    set fechaFin = date_sub(curdate(), interval 7 day);
+    
+	update cliente set puntos = (puntos + (puntos * 0.05) ) where cliente.dni in ( select compra.clienteDni from compra where datetimeCompra between fechaInicio and fechaFin );
+	end;
+//
+
+
+
+create event bonusClienteSemanal on schedule every 1 week starts now() do
+begin 
+	 call bonusClientes();
+     end;
+     
+
+-- Ejercicio 6 --
+
+
+delimiter //
+CREATE PROCEDURE eliminarMarcas_Stock()
+BEGIN 
+    delete from marca where idMarca IN ( SELECT idMarca from ( select prenda.idMarca, SUM(prenda_has_talle.stock) AS total_stock from  prenda 
+     join prenda_has_talle ON prenda.idPrenda = prenda_has_talle.idPrenda 
+		group by  prenda.idMarca ) 
+        where total_stock <= 0 OR total_stock is null
+    )
+    and idMarca not in (
+        select prenda.idMarca
+        from prenda 
+        join compra_detalle on compra_detalle.idPrenda = prenda.idPrenda
+        join compra on compra.idCompra = compra_detalle.idCompra
+        where compra.datetimeCompra >= DATE_SUB(CURDATE(), interval 6 month)
+    );
+end;
+//
+
+create event eliminarStockEvento on schedule every 15 day starts now() do
+begin
+	call eliminarMarcas_Stock();
+end;
