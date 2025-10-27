dee46b1bf7239287b603d819306785cdd8516f75
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2025-10-27 00:44:46 -0300

    modif query con tabla nueva

--- DHT Querys.sql
+++ DHT Querys.sql
@@ -4,12 +4,13 @@
 delimiter //
 create procedure prendasMasVendidas (in fechaInicio date, in fechaFin date)
 begin
-	declare cantVentas int default 0;
-    select count(*) into cantVentas from compra_detalle;
-	select prenda.idPrenda, sum(compra_detalle.cantidad * compra_detalle.precioUnitario) as ingresosGenerados, sum(compra_detalle.cantidad) as cantidadVendida from prenda join compra_detalle on prenda.idPrenda = compra_detalle.idPrenda join compra on compra_detalle.idCompra=compra.idCompra where datetimeCompra between fechaInicio and fechaFin group by prenda.idPrenda order by cantidadVendida desc;
-	end;
-//
-
+	select prenda.idPrenda, prenda.nombre, sum(compra_detalle.cantidad * compra_detalle.precioUnitario) as ingresosGenerados, sum(compra_detalle.cantidad) as cantidadVendida from prenda 
+    join prenda_has_talleycolor p on prenda.idPrenda = p.p_idPrenda
+    join compra_detalle on prenda.idPrenda = compra_detalle.idVariantePrenda  
+    join compra on compra_detalle.idCompra = compra.idCompra 
+    where date(datetimeCompra) between fechaInicio and fechaFin group by prenda.idPrenda, prenda.nombre order by cantidadVendida desc limit 10;
+end//
+delimiter ; 
 call prendasMasVendidas ('2025-10-10','2025-10-12');
 
 
@@ -26,7 +27,6 @@ begin
 
 
 -- Ejercicio 5--
-
 delimiter //
 create procedure bonusClientes () 
 begin
@@ -36,39 +36,39 @@ begin
     set fechaFin = date_sub(curdate(), interval 7 day);
     
 	update cliente set puntos = (puntos + (puntos * 0.05) ) where cliente.dni in ( select compra.clienteDni from compra where datetimeCompra between fechaInicio and fechaFin );
-	end;
-//
-
-
-
-create event bonusClienteSemanal on schedule every 1 week starts now() do
-begin 
-	 call bonusClientes();
-     end;
-     
-
--- Ejercicio 6 --
+end //
+delimiter ;
 
 
 delimiter //
-CREATE PROCEDURE eliminarMarcas_Stock()
+create event bonusClienteSemanal on schedule every 1 week starts now() do
+begin 
+	 call bonusClientes();
+end//
+delimiter ;
+     
+
+-- Ejercicio 6 --
+delimiter //
+create procedure eliminarMarcas_Stock()
 BEGIN 
-    delete from marca where idMarca IN ( SELECT idMarca from ( select prenda.idMarca, SUM(prenda_has_talle.stock) AS total_stock from  prenda 
-     join prenda_has_talle ON prenda.idPrenda = prenda_has_talle.idPrenda 
-		group by  prenda.idMarca ) 
-        where total_stock <= 0 OR total_stock is null
+    delete from marca where idMarca IN ( SELECT idMarca from ( select prenda.idMarca, SUM(prenda_has_talleycolor.stock) AS total_stock from  prenda 
+     join prenda_has_talleycolor ON prenda.idPrenda = prenda_has_talleycolor.idPrenda group by  prenda.idMarca ) 
+	 where total_stock <= 0 OR total_stock is null
     )
     and idMarca not in (
-        select prenda.idMarca
-        from prenda 
-        join compra_detalle on compra_detalle.idPrenda = prenda.idPrenda
+        select prenda.idMarca from prenda 
+        join prenda_has_talleycolor p ON prenda.idPrenda = p.idPrenda 
+        join compra_detalle on compra_detalle.idVariantePrenda = p.p_idPrenda
         join compra on compra.idCompra = compra_detalle.idCompra
         where compra.datetimeCompra >= DATE_SUB(CURDATE(), interval 6 month)
     );
-end;
-//
+END//
+delimiter ;
 
+delimiter //
 create event eliminarStockEvento on schedule every 15 day starts now() do
 begin
 	call eliminarMarcas_Stock();
-end;
+end//
+delimiter ;
